53d3ad75d60b3d5e25458c9b463e972eccaabbfb
Denis5201 <builukden@yandex.ru>
2022-03-28 23:09:33 +0700

    update 1.6

--- js/algo_2.js
+++ js/algo_2.js
@@ -1,1 +1,11 @@
-﻿// JavaScript source code
+﻿window.onload = function () {
+    let canvas = document.querySelector("canvas");
+    let context = canvas.getContext('2d');
+    canvas.onclick = function (e) {
+        context.fillStyle = "#ff0000";
+        let t = canvas.getBoundingClientRect();
+        let x = (e.clientX - t.left) * canvas.width / canvas.clientWidth;
+        let y = (e.clientY - t.top) * canvas.height / canvas.clientHeight;
+        context.fillRect(x, y, 5, 5);
+    }
+}
