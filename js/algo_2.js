1d35f8f6c4cffa741fad86866e014a8243120f9f
Denis5201 <builukden@yandex.ru>
2022-03-31 16:58:17 +0700

    update 2.0

--- js/algo_2.js
+++ js/algo_2.js
@@ -1,11 +1,126 @@
-﻿window.onload = function () {
-    let canvas = document.querySelector("canvas");
-    let context = canvas.getContext('2d');
-    canvas.onclick = function (e) {
-        context.fillStyle = "#ff0000";
+﻿// функция рассчёта расстояний
+function distance(coords1, coords2) {
+    x1 = coords1[0];
+    y1 = coords1[1];
+    x2 = coords2[0];
+    y2 = coords2[1];
+    let dist = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+    return dist;
+}
+
+// здесь функция, которая получает кластер и рассчитывает новый центр
+function newCenter(cluster) {
+    let newCenterX = 0;
+    let newCenterY = 0;
+    for (let i = 1; i < cluster.length; i++) {
+        newCenterX += cluster[i][0];
+        newCenterY += cluster[i][1];
+    }
+    if (cluster.length > 1) {
+        newCenterX /= (cluster.length - 1);
+        newCenterY /= (cluster.length - 1);
+    }
+    else {
+        newCenterX = Math.floor(Math.random() * 500);
+        newCenterY = Math.floor(Math.random() * 500);
+    }
+    let newCenterCoords = [];
+    newCenterCoords.push(newCenterX);
+    newCenterCoords.push(newCenterY);
+    return newCenterCoords;
+}
+
+// центроиды в нулевых элементах кластеров 
+// здесь функция, которая получает центроиды, распихивает точки по кластерам, получает новые центры и сравнивает их с нынешними центроидами. если новые центры и центроиды не совпадают, функция запускает саму себя с новыми центрами в качестве центроид
+function clustering(clusters, arrayOfPoints) {
+    let count = 0;
+    let currentPointCoords;
+    let min;
+    let indexMin;
+    let final;
+    let i;
+    while (true) {
+        for (i = 0; i < arrayOfPoints.length; i++) {
+            currentPointCoords = arrayOfPoints[i];
+            min = distance(clusters[0][0], currentPointCoords);
+            indexMin = 0;
+            for (let j = 1; j < clusters.length; j++) {
+                let check = distance(clusters[j][0], currentPointCoords);
+                if (check < min) {
+                    min = check;
+                    indexMin = j;
+                }
+            }
+            clusters[indexMin].push(arrayOfPoints[i]);
+        }
+
+        final = true;
+        for (i = 0; i < clusters.length; i++) {
+            newCoords = newCenter(clusters[i]);
+            if ((newCoords[0] != clusters[i][0][0]) || (newCoords[1] != clusters[i][0][1])) {
+                clusters[i][0][0] = newCoords[0];
+                clusters[i][0][1] = newCoords[1];
+                final = false;
+            }
+        }
+        if (final || count>1000000) {
+            break;
+        }
+        else {
+            for (i = 0; i < clusters.length; i++) {
+                clusters[i].length = 1;
+            }
+            count++;
+        }
+    }
+    // красим точки
+    for (i = 0; i < clusters.length; i++) {
+        let canvas = document.getElementById('canvas');
+        let ctx = canvas.getContext('2d');
+        ctx.fillStyle = 'rgb(' + Math.round(Math.random() * 255) + ', ' + Math.round(Math.random() * 255) + ', ' + Math.round(Math.random() * 255) + ')';
+        for (let j = 1; j < clusters[i].length; j++) {
+            let x = clusters[i][j][0];
+            let y = clusters[i][j][1];
+            ctx.beginPath();
+            ctx.arc(x, y, 5, 0, Math.PI * 2, true);
+            ctx.fill();
+        }
+    }
+}
+
+// здесь должны считываться точки и записываться в массив
+window.onload = function () {
+    let arrayOfPoints = [];
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+    canvas.onclick = function (event) {
         let t = canvas.getBoundingClientRect();
-        let x = (e.clientX - t.left) * canvas.width / canvas.clientWidth;
-        let y = (e.clientY - t.top) * canvas.height / canvas.clientHeight;
-        context.fillRect(x, y, 5, 5);
+        let x = (event.clientX - t.left) * canvas.width / canvas.clientWidth;
+        let y = (event.clientY - t.top) * canvas.height / canvas.clientHeight;
+        ctx.beginPath();
+        ctx.arc(x, y, 5, 0, Math.PI * 2, true);
+        ctx.fill();
+        let newPoint = [x, y];
+        arrayOfPoints.push(newPoint);
+    }
+    //
+    let button = document.getElementById('startButton');
+    button.onclick = function () {
+        let numberOfClusters = Number(document.getElementById('numberOfClusters').value);
+        if (numberOfClusters > arrayOfPoints.length)
+            alert("Кластеров больше, чем точек");
+        else {
+            let clusters = [];
+            for (let i = 0; i < numberOfClusters; i++) {
+                let newClusterCenter = [
+                    [
+                        Math.floor(Math.random() * 500),
+                        Math.floor(Math.random() * 500)
+                    ]
+                ];
+                clusters.push(newClusterCenter);
+            }
+            clustering(clusters, arrayOfPoints);
+        }
     }
 }
